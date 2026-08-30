a3a8ba92a102da85c22c70660b86302066c927a4
gersonlombacher <gerson@lombacher.de>
2026-08-30 21:18:33 +0200

    V11 Grundstruktur

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vokabeltrainer
-Interaktiver Englisch-Vokabeltrainer
+# VocaFlow V11.0
+
+Neue modulare Web-App-Grundlage. Enthält die 320 Wörter aus Klasse 5 als Wiederholung und einen CSV/TSV-Import für spätere Klasse-6-Units.
--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='vokabeltrainer-v10-3';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="vocaflow-v11-0";const ASSETS=["./","./index.html","./css/style.css","./js/app.js","./js/trainer.js","./js/hints.js","./js/rewards.js","./js/import.js","./data/vokabeln.json","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
